a71bfddd63922b66a9a45da9090210d75aaf4f63
Fathimaan675 <fathimafari675@gmail.com>
2024-03-10 09:45:01 -0700

    quiz application

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const data = [
+    {
+        question: " In Decimal system you can convert the binary number 1011011111000101 very easily.",
+        option1: "Yes",
+        option2: "Hexadecimal number",
+        option3: "Octal system",
+        option4: "both 2 and 3",
+        ans: 2,
+    },
+    {
+        question: "Why to use fflush() library function?",
+        option1: "To flush all streams and specified streams ",
+        option2: "To flush only specified stream",
+        option3: "To flush input/output buffer",
+        option4: " Invalid library function",
+        ans: 1,
+    },
+    {
+        question: "In which language is React.js written",
+        option1: "Python",
+        option2: "JavaScript",
+        option3: "Java",
+        option4: "PHP",
+        ans: 2,
+    },{
+        question: "What is ReactJS mainly used for building",
+        option1: "Database",
+        option2: "Connectivity",
+        option3: "User Interface",
+        option4: "Design Platform",
+        ans: 3,
+    },{
+        question: "Which of the following converts all the statements in a program in a single batch and the resulting collection of instructions is placed in a new file?",
+        option1: "This",
+        option2: "Create",
+        option3: "Inherits",
+        option4: "Extends",
+        ans: 4,
+    }
+]
